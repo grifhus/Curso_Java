75b20cc3da24d5f7d2c780ab5a249d91d50391ac
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-07-20 22:28:58 -0500

    Java: JDBC  Agregando el Driver JAR

--- Base de datos/src/EjemploMain.java
+++ Base de datos/src/EjemploMain.java
@@ -1,4 +1,19 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class EjemploMain {
+    public static void main(String[] args) {
+        Connection conexion;
+        String jdbc = "jdbc:mysql://localhost:3306/ejemplo";
+        try {
+            conexion = DriverManager.getConnection(jdbc,"root","0987654321");
+            //Logica base de datos
+            System.out.println("Estoy dentro");
+        } catch (SQLException sql) {
+            // Tratar esta excepcion
+            sql.printStackTrace();
+        }
+    }
 }
+
